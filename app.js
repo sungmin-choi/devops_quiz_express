bf0154f953452f1f0050a559e0714efbc423f60d
sungmin choi <namja306@naver.com>
2025-01-21 22:47:24 +0900

    feat) questions api

--- app.js
+++ app.js
@@ -10,6 +10,74 @@ const pool = mysql.createPool({
   database: process.env.DB_DATABASE,
 });
 
+app.get("/questions", async (req, res) => {
+  try {
+    const { categoryId, difficultyId, count, mode } = req.query;
+
+    const [rows] = await pool.query(`
+        SELECT 
+            q.*,
+            qo.id AS option_id,
+            qo.*
+        FROM 
+            questions q 
+        LEFT JOIN 
+            question_options qo ON q.id = qo.question_id
+        WHERE 
+            q.category_id = ${categoryId} 
+        ${difficultyId === "3" ? `` : `AND q.difficulty_id = ${difficultyId}`} 
+        ORDER BY q.id
+       `);
+
+    const questions = {};
+
+    rows.forEach((row) => {
+      if (!questions[row.question_id]) {
+        questions[row.question_id] = {
+          questionId: row.question_id,
+          questionText: row.question_text,
+          categoryId: row.category_id,
+          questionTypeId: row.question_type_id,
+          difficultyId: row.difficulty_id,
+          subText: row.sub_text,
+          imageUrl: row.image_url,
+          referenceLink: row.reference_link,
+          explanationText: row.explanation_text,
+          answer: row.answer,
+        };
+        if (row.question_type_id === 1 || row.question_type_id === 2) {
+          questions[row.question_id].options = [];
+        } else {
+          questions[row.question_id].options = null;
+        }
+      }
+      if (row.option_id) {
+        questions[row.question_id].options.push(row.option_text);
+      }
+    });
+
+    var result = Object.values(questions).map((question) => {
+      if (question.options) {
+        question.options = question.options.sort(() => Math.random() - 0.5);
+      }
+      return question;
+    });
+
+    if (mode === "random") {
+      result.sort(() => Math.random() - 0.5);
+    }
+
+    if (parseInt(count)) {
+      result = result.slice(0, parseInt(count));
+    }
+
+    res.json(result);
+  } catch (err) {
+    console.error("Database Error:", err);
+    res.status(500).json({ error: err.message });
+  }
+});
+
 app.get("/categories", async (req, res) => {
   try {
     const [rows] = await pool.query(`SELECT
